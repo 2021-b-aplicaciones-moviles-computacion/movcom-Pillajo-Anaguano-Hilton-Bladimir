ae4c6217276e0801de5a5eae2fcdbd92e208f288
BladimirPillajo <94177234+BladimirPillajo@users.noreply.github.com>
2021-12-13 23:42:08 -0500

    Update paciente.kt

--- medico-paciente/src/main/kotlin/paciente.kt
+++ medico-paciente/src/main/kotlin/paciente.kt
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane
 class Paciente(
     var cedula: String,
     var nombre: String,
-    var telefono: Long,
+    var telefono: String,
     var edad: Int,
     var pacientefrecuente: Boolean = true
 ) {
@@ -20,6 +20,73 @@ fun crearPaciente(
         medicos[indMedico].pacientes?.add(paciente)
     }
     return medicos
+}
+
+fun eliminarPaciente(
+    cedula: String,
+    medicos: MutableList<Medico>
+): MutableList<Medico> {
+    val ind = RetornarIndices(cedula, medicos)
+    val existeMedico = ind["medico"]!! > -1
+    val existePaciente = ind["paciente"]!! > -1
+
+    if (existeMedico && existePaciente) {
+        val indMedico = ind["medico"] as Int
+        val indPaciente = ind["paciente"] as Int
+        medicos[indMedico].pacientes?.removeAt(indPaciente)
+    }
+    return medicos
+}
+
+fun editarPaciente(
+    cedula: String,
+    campoEditar: String,
+    nuevoValor: String,
+    medicos: MutableList<Medico>
+): MutableList<Medico> {
+    val ind = RetornarIndices(cedula, medicos)
+    val existeMedico = ind["medico"]!! > -1
+    if (existeMedico) {
+        val indMedico = ind["medico"] as Int
+        val indPaciente = ind["paciente"] as Int
+        when (campoEditar) {
+            "telefono" -> {
+                medicos[indMedico].pacientes?.get(indPaciente)?.telefono = nuevoValor
+            }
+            "pacientefrecuente" -> {
+                medicos[indMedico].pacientes?.get(indPaciente)?.pacientefrecuente = nuevoValor.toBoolean()
+            }
+        }
+    }
+    return medicos
+}
+
+fun buscarPaciente(
+    parametro: String,
+    datoConsulta: String,
+    listaMedicos: MutableList<Medico>
+): List<List<Any>?> {
+    var medicoEncontrado: List<List<Any>?> = emptyList()
+    when (parametro) {
+        "nombre" -> {
+            medicoEncontrado = listaMedicos
+                .map { medico: Medico ->
+                    val medicoYpaciente = medico.pacientes?.filter { paciente: Paciente ->
+                        return@filter paciente.nombre == datoConsulta
+                    }
+                    return@map medicoYpaciente?.let { listOf<Any>(medico, it) }
+                }.filter { list: List<Any>? ->
+                    return@filter list != null
+                }.filter { list ->
+                    val pacientefiltrado = list?.get(1) as List<Paciente>
+                    return@filter pacientefiltrado.size > 0
+                }
+        }
+        else -> {
+            JOptionPane.showMessageDialog(null, "Busqueda por ${parametro} no fue encontrado")
+        }
+    }
+    return medicoEncontrado
 }
 
 fun RetornarIndices(
