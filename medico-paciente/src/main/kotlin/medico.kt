16c3fd8f53e1dd309fcfe6f07c9205a3d1eae6e3
BladimirPillajo <94177234+BladimirPillajo@users.noreply.github.com>
2021-12-13 23:41:51 -0500

    Update medico.kt

--- medico-paciente/src/main/kotlin/medico.kt
+++ medico-paciente/src/main/kotlin/medico.kt
@@ -1,6 +1,6 @@
+import java.util.*
 import java.io.File
 import java.io.InputStream
-import java.util.*
 import javax.swing.JOptionPane
 
 class Medico(
@@ -24,6 +24,63 @@ fun crearMedico(
     val pacientes = mutableListOf<Paciente>()
     return mutableListOf(Medico(ruc, nombre,telefono, fechanacimiento, precioconsulta, pacientes = pacientes))
 }
+fun eliminarMedico(
+    nombre: String,
+    medicos: MutableList<Medico>
+): MutableList<Medico> {
+    val ind = RetornarIndice(nombre, medicos)
+    val existeEmpresa = ind > -1
+    if (existeEmpresa) {
+        medicos.removeAt(ind)
+    }
+    return medicos
+}
+fun editarMedico(
+    nombre: String,
+    datoEditar: String,
+    nuevoValor: String,
+    medicos: MutableList<Medico>
+): MutableList<Medico> {
+    val ind = RetornarIndice(nombre, medicos)
+    val existeMedico = ind > -1
+    if (existeMedico) {
+        when (datoEditar) {
+            "nombre" -> {
+                medicos[ind].nombre = nuevoValor
+            }
+            "ruc" -> {
+                medicos[ind].ruc = nuevoValor
+            }
+            "telefono" -> {
+                medicos[ind].telefono = nuevoValor.toInt()
+            }
+            else -> {
+                JOptionPane.showMessageDialog(null, "No se encontro el Medico  ${datoEditar}")
+            }
+        }
+    }
+    return medicos
+}
+
+fun buscarMedico(
+    parametro: String,
+    datoConsulta: String,
+    medicos: MutableList<Medico>
+): List<Medico> {
+    var medicoEncontrado: List<Medico> = emptyList()
+    when (parametro) {
+        "nombre" -> {
+            medicoEncontrado = medicos
+                .filter { medico: Medico ->
+                    return@filter medico.nombre == datoConsulta
+                }
+        }
+        else -> {
+            JOptionPane.showMessageDialog(null,"Busqueda de  ${parametro} no encontrado")
+        }
+    }
+    return medicoEncontrado
+}
 
 fun RetornarIndice(
     nombre: String,
@@ -38,5 +95,10 @@ fun RetornarIndice(
         return -1
     }
     return medicos.indexOf(respuesta[0])
+}
 
+fun listarInformacion(): String {
+    val inputStream: InputStream = File("BaseDatos.txt").inputStream()
+    val inputString = inputStream.bufferedReader().use { it.readText() }
+    return inputString
 }
